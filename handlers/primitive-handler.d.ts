aff12c61bdbd312209f61e8dc78e570bff493389
Ariel <ariellichter@gmail.com>
2019-06-23 14:06:59 +0300

    feat(.gitignore): added

--- handlers/primitive-handler.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrimitiveCollection } from 'cesium';
-export default class PrimitiveHandler {
-    private readonly meaningfulFields;
-    constructor();
-    getPrimitivesEntitiesRecursively(primitiveCollection: PrimitiveCollection, resultTree?: any, lastNameCounter?: number): any;
-    private handlePrimitive;
-    private isPrimitiveFieldNotEmpty;
-}
